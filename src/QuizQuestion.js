07dc93ae4d41e2bbae014558a344d289cd1e1779
siva-1234 <71683191+siva-1234@users.noreply.github.com>
2022-11-04 18:26:38 +0530

    Add files via upload

---
+++ src/QuizQuestion.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton.js";
+class QuizQuestion extends Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>
+              <QuizQuestionButton
+                button_text={this.props.quiz_question.answer_options[0]}
+              />
+            </li>
+          </ul>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default QuizQuestion;
