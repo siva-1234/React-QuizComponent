089d34039358968ed5ad93dd81334ce94b86898d
siva-1234 <71683191+siva-1234@users.noreply.github.com>
2022-11-05 06:59:11 +0530

    Add files via upload

---
+++ src/Quiz.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import QuizEnd from "./QuizEnd";
+import QuizQuestion from "./QuizQuestion";
+let quizData = require("./quiz_data.json");
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
+  render() {
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
+      </div>
+    );
+  }
+}
+export default Quiz;
